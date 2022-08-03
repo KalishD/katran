c02cf7d652292d6b8f3cacb7fc99a38d2278881e
KalishD <kalish.dmitry@gmail.com>
2022-08-03 16:30:48 +0300

    Static Pages

--- apps/core/views.py
+++ apps/core/views.py
@@ -17,17 +17,26 @@ def production(request):
     mp006 = Product.objects.filter(sku = 1011040).first()
     mp01122 = Product.objects.filter(sku = 1011030).first()
     mp01115 = Product.objects.filter(sku = 1011020).first()
+    mp011s = Product.objects.filter(sku = 1010140).first()
     rm8 = Product.objects.filter(sku = 1040560).first()
     rm12 = Product.objects.filter(sku = 1040550).first()
     rm16 = Product.objects.filter(sku = 1040540).first()
+    mp011_list = [mp01115,mp01122,mp011s]
+    rm_list = [rm8,rm12,rm16]
+    # mp011_list.append(mp01115)
+    # mp011_list.append(mp01122)
+    # mp011_list.append(mp011s)
 
     context = {
         'mp006': mp006,
         'mp01122': mp01122,
         'mp01115': mp01115,
+        'mp011s':  mp011s,
         'rm8': rm8,
         'rm12': rm12,
         'rm16': rm16,
+        'mp011_list': mp011_list,
+        'rm_list': rm_list,
     }
 
     return render(request, 'production.html', context)
