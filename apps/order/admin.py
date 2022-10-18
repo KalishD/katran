14408e215119a411a2411fd749e9e4e82f686f76
KalishD <kalish.dmitry@gmail.com>
2022-10-18 12:02:07 +0300

    1233

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -15,12 +15,13 @@ class OrderAdmin(admin.ModelAdmin):
   def item_count(self,obj):
     count = OrderItem.objects.filter(order=obj).count()
     return count
+  item_count.short_description = "Products"
   def order_sum(self,obj):
     sum = 0
     for order_item in OrderItem.objects.filter(order=obj):
       sum += order_item.price
-    
     return sum
+  order_sum.short_decription = "Total price"
     
   list_display = ("name","email","phone","address", "created_at", "item_count", "order_sum")
   inlines = [OrderItemInline]
